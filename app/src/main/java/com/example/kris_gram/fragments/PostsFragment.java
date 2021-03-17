d78797eb64c136058fa701ae9173043019440f51
Kristina Ilyovska <lyloster@gmail.com>
2021-03-17 14:08:07 -0400

    Swipe to refresh

--- app/src/main/java/com/example/kris_gram/fragments/PostsFragment.java
+++ app/src/main/java/com/example/kris_gram/fragments/PostsFragment.java
@@ -7,6 +7,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -29,7 +30,7 @@ public class PostsFragment extends Fragment {
     private RecyclerView rvPosts;
     protected PostsAdapter adapter;
     protected List<Post> allPosts;
-
+    private SwipeRefreshLayout swipeContainer;
     public PostsFragment() {
         // Required empty public constructor
     }
@@ -50,6 +51,23 @@ public class PostsFragment extends Fragment {
         rvPosts.setAdapter(adapter);
         rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
         queryPost();
+
+        swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                queryPost();
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
     }
 
     protected void queryPost() {
@@ -69,9 +87,9 @@ public class PostsFragment extends Fragment {
                 for (Post post: posts) {
                     Log.i(TAG, "Post description: " + post.getDescription() + ", username: " + post.getUser().getUsername());
                 }
-
                 allPosts.addAll(posts);
                 adapter.notifyDataSetChanged();
+                swipeContainer.setRefreshing(false);
             }
         });
     }
